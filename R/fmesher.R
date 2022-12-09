a1fbe8c38323ec393a4c52f25d7a64661ded5835
Finn Lindgren <Finn.Lindgren@gmail.com>
2022-12-09 14:55:52 +0000

    More namespace fixes

--- R/fmesher.R
+++ R/fmesher.R
@@ -31,7 +31,7 @@ fm_as_dgTMatrix <- function(x, unique = TRUE) {
 fm_qinv <- function(A) {
   A_C <- fm_as_dgCMatrix(A)
   stopifnot(nrow(A_C) == ncol(A_C))
-  if (!identical(A_C, t(A_C))) {
+  if (!identical(A_C, Matrix::t(A_C))) {
     warning("Asymmetric matrix A detected, but only lower left triangle will be used.")
   }
   C_qinv(A_C)
