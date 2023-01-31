5880ac7fc0a822f177de530c605ed69b6b495a3f
Finn Lindgren <Finn.Lindgren@gmail.com>
2023-01-31 22:42:59 +0000

    qinv speed checks

--- misc/qinv_check.R
+++ misc/qinv_check.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(fmesher)
 library(INLA)
 library(Matrix)
-d_ <- c(10, 32, 100, 320, 1000, 3200)
+d_ <- c(10, 32, 100, 320, 1000)#, 3200)
 prob_ <- c(1, 0.5, 0.2,
            0.1, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.038, 0.036, 0.034, 0.032, 0.03,
            0.02, 0.018, 0.016, 0.014, 0.012,
@@ -10,6 +10,9 @@ prob_ <- c(1, 0.5, 0.2,
 prob_ <- c(1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.002, 0.001)
 prob_C_ <- NULL
 result <- list()
+progressr::handlers("cli")
+progressr::with_progress({
+  pr <- progressr::progressor(length(d_) * length(prob_))
 for (dim in d_) {
   for (prob in prob_) {
     A <- Matrix(rnorm(dim^2), dim, dim)
@@ -19,10 +22,16 @@ for (dim in d_) {
     mean(C != 0)
     prob_C_ <- c(prob_C_, mean(C != 0))
     result <- c(result,
-                list(bench::mark(fm_qinv(C), solve(C), inla.qinv(C), check = FALSE)
+                list(bench::mark(fm_qinv(C),
+                                 solve(C),
+                                 inla.qinv(C),
+                                 check = FALSE,
+                                 iterations = 1)
                      ))
+    pr()
   }
 }
+})
 result <- do.call(rbind, result)
 result$prob <- rep(rep(prob_, each = 3), times = length(d_))
 result$prob_C <- rep(prob_C_, each = 3)
---
+++ misc/qinv_speed_check.R
@@ -0,0 +1,73 @@
+library(fmesher)
+Qinv <- function(Q){
+  fmesher:::C_qinv(as(as(as(Q, "Matrix"), "generalMatrix"), "CsparseMatrix"))
+}
+Qinv_ <- function(Q){
+  fmesher:::C_qinv(Q)
+}
+data <- NULL
+progressr::handlers("cli")
+progressr::with_progress({
+  N_ <- c(1:30, 40, 50, 75, 100)
+  N_ <- rev(c(1, 2, 4, 8, 16, 32, 64, 128))
+  N_ <- rev(c(1, 2, 4, 8, 16, 32))
+  pr <- progressr::progressor(2 * length(N_))
+  n <- c()
+  for (N in N_) {
+    m <- INLA::inla.mesh.create(globe = N)
+    n <- c(n, m$n)
+    for (car_order in 1:2) {
+      fem <- INLA::inla.mesh.fem(m)
+      if (car_order == 1) {
+        Q <- fem$g1 + fem$c0
+      } else {
+        Q <- fem$g2 + fem$c0
+      }
+      Q_ <- as(as(Q, "generalMatrix"), "CsparseMatrix")
+      timing <-
+        bench::mark(S_fm = Qinv(Q),
+                    S_fm1 = Qinv_(Q_),
+                    S_inla = INLA::inla.qinv(Q),
+                    S_inla1 = INLA::inla.qinv(Q, reordering = "amd"),
+                    check = FALSE)
+      data <- rbind(data,
+                    data.frame(n = m$n,
+                               N = N,
+                               time = as.numeric(timing$median),
+                               method = c("fm", "fm1", "inla", "inla1"),
+                               car = car_order)
+      )
+      pr()
+    }
+  }
+})
+
+library(tidyverse)
+
+fit <- list(list(), list())
+for (car_order in 1:2) {
+  for (method_ in unique(data$method)) {
+    fit[[car_order]][[method_]] <- lm(time~I(n/1e3)+I(n^2/1e6)+I(n^3/1e9)+I(n^4/1e12),
+                                      data = data %>% filter(method == method_, car==car_order))
+    data[data$method == method_ & data$car == car_order, "fit"] <-
+      predict(fit[[car_order]][[method_]],
+              newdata = data.frame(n = n, N = N_))
+  }
+}
+
+pl <-
+  ggplot(data) +
+  geom_point(aes(n, time, col = method)) +
+  facet_wrap(vars(car)) +
+  scale_x_log10() +
+  scale_y_log10() +
+  geom_line(aes(n, fit, col = method))
+pl
+pl <-
+  ggplot(data) +
+  geom_line(aes(n, time, col = method)) +
+  facet_wrap(vars(car)) +
+  scale_x_log10() +
+  scale_y_log10()
+pl
+
