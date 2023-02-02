e558e61fc3a0aed2f0c2496c1d9697d360860a4a
Finn Lindgren <Finn.Lindgren@gmail.com>
2023-02-02 15:56:18 +0000

    Add fm_bary method

--- R/evaluator.R
+++ R/evaluator.R
@@ -111,59 +111,12 @@ fm_evaluator <- function(...) {
 fm_evaluator_inla_mesh <- function(mesh, loc = NULL, crs = NULL, ...) {
   stopifnot(inherits(mesh, "inla.mesh"))
 
-  # Support INLA <= 22.11.27 by converting globes to spheres
-  # TODO: Handle the > 22.11.27 more efficiently
-  if (!fm_crs_is_null(mesh$crs)) {
-    if (fm_crs_is_geocent(mesh$crs)) {
-      crs.sphere <- fm_CRS("sphere")
-      if (!fm_identical_CRS(mesh$crs, crs.sphere)) {
-        ## Convert the mesh to a perfect sphere.
-        mesh <- fm_transform(mesh, crs = crs.sphere)
-      }
-      if (!is.matrix(loc)) {
-        if (!fm_identical_CRS(crs, crs.sphere)) {
-          loc <- fm_transform(loc, crs = crs.sphere, crs0 = crs)
-        }
-      } else {
-        if (fm_crs_is_null(crs)) {
-          loc <- loc / rowSums(loc^2)^0.5
-        } else {
-          loc <- fm_transform(loc, crs = crs.sphere, crs0 = crs)
-        }
-      }
-    } else if (!fm_identical_CRS(crs, mesh$crs)) {
-      mesh <- fm_transform(mesh, crs = mesh$crs, crs0 = crs, passthrough = TRUE)
-    }
-  } else if (identical(mesh$manifold, "S2")) {
-    mesh$loc <- mesh$loc / rowSums(mesh$loc^2)^0.5
-    loc <- loc / rowSums(loc^2)^0.5
-  }
-
-  if (inherits(loc, c("SpatialPoints", "SpatialPointsDataFrame"))) {
-    loc <- sp::coordinates(loc)
-  } else if (inherits(loc, c("sf", "sfc", "sfg"))) {
-    loc <- sf::st_coordinates(loc)
-    c_names <- colnames(loc)
-    c_names <- intersect(c_names, c("X", "Y", "Z"))
-    loc <- loc[, c_names, drop = FALSE]
-  }
-
-  jj <-
-    which(rowSums(matrix(is.na(as.vector(loc)),
-      nrow = nrow(loc),
-      ncol = ncol(loc)
-    )) == 0)
-  smorg <- (INLA::inla.fmesher.smorg(mesh$loc,
-    mesh$graph$tv,
-    points2mesh = loc[jj, , drop = FALSE]
-  ))
+  smorg <- fm_bary(mesh, loc = loc)
   ti <- matrix(0L, nrow(loc), 1)
-  b <- matrix(0, nrow(loc), 3)
-  ti[jj, 1L] <- smorg$p2m.t
-  b[jj, ] <- smorg$p2m.b
+  ti[, 1L] <- smorg$t
+  b <- smorg$bary
 
-  ok <- (ti[, 1L] > 0L)
-  ti[ti[, 1L] == 0L, 1L] <- NA
+  ok <- !is.na(ti[, 1L])
 
   ii <- which(ok)
   A <- (Matrix::sparseMatrix(
