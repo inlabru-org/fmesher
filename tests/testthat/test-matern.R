6346cceb752865dcc574088789be1f333711bb68
Liam Llamazares <s2266126@ed.ac.uk>
2023-10-20 13:23:37 +0100

    Removed files and tests from other package

--- tests/testthat/test-matern.R
+++ tests/testthat/test-matern.R
@@ -32,22 +32,3 @@ test_that("GMRF methods, 2d", {
   expect_equal(dim(samp), c(fm_dof(mesh), 2))
   expect_equal(colSums(samp_constr), c(2, 2))
 })
-
-test_that("GMRF methods, anisotropic", {
-  mesh <- fm_mesh_2d_inla(
-    boundary = fm_extensions(cbind(2, 1), 3),
-    max.edge = 0.5
-  )
-
-  Q <- fm_aniso_precision(mesh, list(rep(1, mesh$n), matrix(1, mesh$n, 2)))
-  expect_equal(dim(Q), c(1, 1) * fm_dof(mesh))
-
-  set.seed(12345L)
-  samp <- fm_sample(n = 2, Q = Q)
-  expect_equal(dim(samp), c(fm_dof(mesh), 2))
-
-  set.seed(12345L)
-  samp_constr <- fm_sample(n = 2, Q = Q, constr = list(A = matrix(1, 1, fm_dof(mesh)), e = 2))
-  expect_equal(dim(samp), c(fm_dof(mesh), 2))
-  expect_equal(colSums(samp_constr), c(2, 2))
-})
