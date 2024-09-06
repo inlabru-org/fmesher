ece260d4a8786905fbbb08e22527639e90056c23
Finn Lindgren <Finn.Lindgren@gmail.com>
2024-09-06 15:05:43 +0100

    Add license information to sourcefiles

--- misc/src_standalone/x11utils.h
+++ misc/src_standalone/x11utils.h
@@ -1,3 +1,11 @@
+/*
+ *  Copyright Finn Lindgren (2010-2024)
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public License,
+ *  v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ *  obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef _FMESH_X11UTILS_
 #define _FMESH_X11UTILS_ 1
 #ifdef FMESHER_WITH_X
--- src/mesh.h
+++ src/mesh.h
@@ -1,3 +1,11 @@
+/*
+ *  Copyright Finn Lindgren (2010-2024)
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public License,
+ *  v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ *  obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef _FMESH_MESH_
 #define _FMESH_MESH_ 1
 
